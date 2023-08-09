5d501ce138873256ca7e482fc1326354c37b93d2
Willian Rodrigues <w.almeida.w@outlook.com>
2023-08-09 20:26:31 -0300

    Classe temporária de envio de emails

---
+++ src/lib/mail/index.ts
@@ -0,0 +1,9 @@
+class Mailer{
+    static Send( templateTag : string, content : any ){
+        const timestamp = `E-mail ${ templateTag } - ${ new Date().getTime() }`;
+        console.groupCollapsed(timestamp)
+        console.log("E-mail enviado:")
+        console.log(content)
+        console.groupEnd()
+    }
+}
