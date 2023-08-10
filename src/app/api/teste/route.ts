64f914dd820d9a4d8f9dcda34c94ff2cbbcc3aa5
Willian Rodrigues <w.almeida.w@outlook.com>
2023-08-10 17:33:54 -0300

    Update route.ts

--- src/app/api/teste/route.ts
+++ src/app/api/teste/route.ts
@@ -1,46 +1,54 @@
-import { Account } from "@/lib/controller/account";
-import { Namespace } from "@/lib/controller/namespace";
-import { User } from "@/lib/controller/user";
+// import { User } from "@/lib/controller/user";
 import { NextResponse, type NextRequest } from "next/server";
 
-export async function GET(request: NextRequest){  
+export async function GET(request: NextRequest){
+    
     // Cria um usuário  
-    // const user = await User.create({
+    // await User.create({
     //     name: "Willian Rodrigues",
     //     email: "okaywillian@gmail.com",
     //     password: "12345678"
     // })
 
-    let user:User = await User.get(1);
+    // let user:User = await User.get(1);
 
     // Cria um namespace
     // const namespace = await user.createNamespace('wln','','Wlln Bank',0);
 
-    const limits = await user.getLimit();
-    const namespaces = await user.getNamespaces();
+    // const limits = await user.getLimit();
+    // const namespaces = await user.getNamespaces();
 
-    // const token = await user.createAcessToken(namespaces[0].id, new Date('01/01/2025'))
-    const token = {
-        "key": "mimecoin_0D77ZAix8Xt7tJph",
-        "secret": "0D77ZAixU5plG6jAXTp0Hk5e"
-    }
+    // const token = await user.createAcessToken(namespace.id, new Date('01/01/2025'))
+    
+    // LOCALHOST
+    // const token = {
+    //     "key": "mimecoin_0D77ZAix8Xt7tJph",
+    //     "secret": "0D77ZAixU5plG6jAXTp0Hk5e"
+    // }
 
-    const namespace_limits = [];
-    for(const name of namespaces ){
-        namespace_limits.push({
-            namespace: name,
-            limits: await name.getLimits()
-        })
-    }
 
-    return NextResponse.json({
-        data: {
-            user,
-            limits : limits,
-            namespaces: namespace_limits,
-            token
-        },
-        message: "Dados",
-        timestamp: new Date().getTime()
-    });
+    // VERCEL
+    // const token = {
+    //     "key": "mimecoin_0D7DMOg4uqmhtNmk",
+    //     "secret": "0D7DMOg4MNHrkaU4BmX6DR2G"
+    //   }
+
+    // const namespace_limits = [];
+    // for(const name of namespaces ){
+    //     namespace_limits.push({
+    //         namespace: name,
+    //         limits: await name.getLimits()
+    //     })
+    // }
+
+    // return NextResponse.json({
+    //     data: {
+    //         user,
+    //         limits : limits,
+    //         namespaces: namespace_limits,
+    //         token
+    //     },
+    //     message: "Dados",
+    //     timestamp: new Date().getTime()
+    // });
 }
