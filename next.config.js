442e31d4bcb56705e7e3830c45e861154ff1c1ca
Willian Rodrigues <w.almeida.w@outlook.com>
2023-08-25 19:12:50 -0300

    Remove cors

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-        { key: "Access-Control-Allow-Credentials", value: "true" },
-        { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "*" },
-        ]
-      }
-    ]
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
