c269f1d44a1a2a6bdf07572460939473fcd079fc
Willian Rodrigues <w.almeida.w@outlook.com>
2023-08-30 15:28:42 -0300

    cors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "*" },
+          ]
+        }
+      ]
+    },
     experimental: {
       serverActions: true,
     },
