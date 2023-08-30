a44c78d549ddf37964b7ef3cf3434ba160fa379e
Willian Rodrigues <w.almeida.w@outlook.com>
2023-08-30 11:26:28 -0300

    Correção de responsividade da home

--- src/app/features.component.tsx
+++ src/app/features.component.tsx
@@ -12,7 +12,7 @@ export default function HeaderMenu() {
 
   return <div className={ `flex flex-col gap-3 relative ${ active ? 'bg-primary' : '' }` }>
     <div className="flex flex-col gap-3 pt-10 px-10">
-      <aside className="flex flex-row gap-3 items-center px-[20px]">
+      <aside className="flex flex-col sm:flex-row gap-3 items-center px-[20px]">
         <h1>
           <Image src={ active ? '/assets/logo-white.svg' : '/assets/logo.svg' } 
             sizes="100vw"
@@ -29,12 +29,12 @@ export default function HeaderMenu() {
           <button className={ `py-3 px-5 flex text-left rounded-full transition-background text-xs text-primary-foreground ${ active ? 'bg-primary hover:bg-primary-500' : 'hover:bg-foreground-700' }` } onClick={() => signIn()}>Login</button>
           <ButtonLink auth="all" href="/dashboard" style={ active ? 'bg-foreground-900 hover:bg-foreground-700' : 'bg-primary hover:bg-primary-500' }>criar seu mime</ButtonLink>
           <button onClick={ () => setStatus( !active ) } className={ `py-3 px-5 flex text-left rounded-full transition-background text-xs text-primary-foreground bg-primary hover:bg-primary-500` }>
-            <span className="material-icon">{ active ? 'close' : 'menu' }</span>
+            <span className="material-icon">{ active ? 'close' : 'star' }</span>
           </button>
         </div>
       </aside>
     </div>
-    { active && <div className="absolute top-[100%] w-full flex flex-row bg-primary p-10 z-50 gap-6 justify-center">
+    { active && <div className="absolute top-[100%] w-full flex flex-row flex-nowrap bg-primary p-10 z-50 gap-6 md:justify-center overflow">
       <Feat title="banco completo" details="transferência entre contas, depósito, transferência e transações" />
       <Feat title="conta digital" details="cada usuário do seu banco tem sua própria conta digital" />
       <Feat title="gerencie os recursos" details="você decide o que seu usuário pode fazer" />
@@ -45,8 +45,8 @@ export default function HeaderMenu() {
 }
 
 function Feat({ title, details, link }: { title: string, details: string, link?: string }){
-  return <div className="flex flex-col p-6 bg-foreground-900 aspect-[192/270] w-48 rounded-[2.5rem] transition-background hover:bg-primary-500 gap-3">
-    <div className="text-2xl font-black leading-none">{ title }</div>
+  return <div className="flex flex-col p-6 bg-foreground-900 aspect-[192/270] w-48 rounded-[2.5rem] transition-background hover:bg-primary-500 gap-3 shrink-0">
+    <div className="text-xl sm:text-2xl font-black leading-none">{ title }</div>
     <div className="text-sm leading-normal font-light">{ details }</div>
     { link ? <Link href={link}>ver mais</Link> : <></> }
   </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ export default async function Home() {
   return (<div className="bg-primary">
     <div className="bg-white rounded-t-[40px] mt-4 flex flex-col gap-10">
       <HeaderMenu />
-      <div className="p-10 flex flex-col gap-10">
+      <div className="px-10 flex flex-col gap-10">
         <header className="relative flex gap-3 flex-col">
-          <div className="flex w-full gap-3 flex-col relative rounded-[40px] aspect-[16/9] bg-cover" style={{ backgroundImage: 'url(/assets/home/cover.png)' }} ></div>
+          <div className="flex gap-3 flex-col relative rounded-[40px] bg-cover bg-center" style={{ backgroundImage: 'url(/assets/home/cover.png)', minHeight: 'max(calc(100vh - 15rem), 480px)' }} ></div>
           <span className="text-right text-xs">*Mimecoin não transaciona dinheiro real</span>
-          <div className="absolute top-20 left-20 max-w-[40%] text-white flex flex-col gap-4 items-start">
-            <div className="text-6xl leading-[1.1]">O primeiro banco virtual digital*</div>
-            <p className="w-[55%]">Ideal para pontos de fidelidade, pontos de recompensa e outras aplicações de tokenização.</p>
+          <div className="absolute top-0 left-0 p-10 md:p-20 max-w-xl text-white flex flex-col gap-4 items-start">
+            <div className="text-5xl sm:text-6xl leading-[1.1] drop-shadow-md">O primeiro banco virtual digital*</div>
+            <p className="sm:max-w-md text-xl drop-shadow-md">Ideal para pontos de fidelidade, pontos de recompensa e outras aplicações de tokenização.</p>
             <ButtonLink auth="all" href="/" style="bg-primary hover:bg-primary-600">criar seu mime</ButtonLink>
           </div>
         </header>
