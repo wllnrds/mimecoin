7215747b0edc86f13ee285fb1fa37036d2ef038a
Willian Rodrigues <w.almeida.w@outlook.com>
2023-09-16 18:08:16 -0300

    Google Tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import '@/style/globals.scss'
 
-import Provider from './providers'
 import type { Metadata } from 'next'
+import Script from 'next/script'
+import Provider from './providers'
+
 import 'moment/locale/pt-br';
  
 export const metadata: Metadata = {
@@ -15,6 +17,16 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </head>
       <body className='dark'>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-95NEKER5JB" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', 'G-95NEKER5JB');
+          `}
+        </Script>
         <Provider>
             <div className='h-full flex flex-col'>
               { children }
