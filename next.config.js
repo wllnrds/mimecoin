203d89fb4eb53ab4b2e1b84d8aa360c4823d7f5a
Willian Rodrigues <w.almeida.w@outlook.com>
2024-02-01 15:36:21 -0300

    Upload txt do Loader.io

--- next.config.js
+++ next.config.js
@@ -12,16 +12,6 @@ const nextConfig = {
             { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
             { key: "Access-Control-Allow-Headers", value: "*" },
           ]
-        },
-        {
-          source: "/loaderio-ddf1cca9fa336591beb191ccd6b03a9a",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Content-Type", value : "*/*; charset=utf-8" },
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "*" },
-          ]
         }
       ]
     },
--- src/app/loaderio-ddf1cca9fa336591beb191ccd6b03a9a/route.ts
+++
@@ -1,3 +0,0 @@
-export async function GET() {
-  return new Response("loaderio-ddf1cca9fa336591beb191ccd6b03a9a")
-}
